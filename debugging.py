1eb4c68d621ea342b24e5c826a2edb2525f9d9aa
Gabriel Zarate <gzaratet@sct.gob.mx>
2022-07-22 16:45:02 -0500

    the challenge to handle except and raise in Python

--- debugging.py
+++ debugging.py
@@ -1,19 +1,27 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num <= 0:
+            raise ValueError("Ingrese un número mayor que cero")
+        divisors = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return []
 
 
 def run():
-    try:
-        num = int(input("Ingresa un número: "))
-        print(divisors(num))
-    except ValueError:
-        print("Debes ingresar un número")
+    while True:
+        try:
+            num = int(input("Ingresa un número: "))
+            print(divisors(num))
+            print("Terminó el programa")
+            break
 
-    print("Terminó el programa")
+        except ValueError:
+            print("Debes ingresar un número")
 
 
 if __name__ == '__main__':
